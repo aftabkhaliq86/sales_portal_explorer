7521da527658bea5a11a3707e1c91566e98b6a1e
AzamNaveed <agent179@remitchoice.email>
2023-05-10 14:15:32 +0500

    update the calling lead v1 to calling lead

--- calling_lead.php
+++ calling_lead.php
@@ -138,7 +138,13 @@ if (isset($_REQUEST['actc'])) {
     echo ("<script>location='" . basename($_SERVER['PHP_SELF']) . "'</script>");
 }
 ?>
-
+<style>
+    #calling-loader {
+        text-align: center;
+        font-weight: bold;
+        font-size: 20px;
+    }
+</style>
 
 <body class="nav-md">
     <div class="container body">
@@ -184,153 +190,38 @@ if (isset($_REQUEST['actc'])) {
 
                                     ?>
                                     <div class="col-lg-5">
-                                        <a href="calling_lead.php?sts=1" class="col-lg-12 btn <?php if ($sts == 1) {
-                                                                                                    echo 'btn-success';
-                                                                                                } else {
-                                                                                                    echo 'btn-default';
-                                                                                                } ?>  al-center">
+                                        <a href="<?php echo $_SERVER['PHP_SELF']; ?>?sts=1" class="col-lg-12 btn <?php if ($sts == 1) {
+                                                                                                                        echo 'btn-success';
+                                                                                                                    } else {
+                                                                                                                        echo 'btn-default';
+                                                                                                                    } ?>  al-center">
                                             <h5><i class="fa fa-check"></i> Active</h5>
                                         </a>
                                     </div>
                                     <div class="col-lg-5">
-                                        <a href="calling_lead.php?sts=2" class="col-lg-12 btn <?php if ($sts == 2) {
-                                                                                                    echo 'btn-success';
-                                                                                                } else {
-                                                                                                    echo 'btn-default';
-                                                                                                } ?>  al-center">
+                                        <a href="<?php echo $_SERVER['PHP_SELF']; ?>?sts=2" class="col-lg-12 btn <?php if ($sts == 2) {
+                                                                                                                        echo 'btn-success';
+                                                                                                                    } else {
+                                                                                                                        echo 'btn-default';
+                                                                                                                    } ?>  al-center">
                                             <h5><i class="fa fa-times"></i> InActive</h5>
                                         </a>
                                     </div>
                                     <div class="col-lg-2">
-                                        <a href="calling_lead.php" class="col-lg-12 btn btn-default al-center">
+                                        <a href="<?php echo $_SERVER['PHP_SELF']; ?>" class="col-lg-12 btn btn-default al-center">
                                             <h5><i class="fa fa-times"></i></h5>
                                         </a>
                                     </div>
                                 </div>
-
-                                <div class="x_content">
-                                    <?php
-                                    if (isset($_POST['srchfilter'])) //submit button name
-                                    {
-                                        $srch_DATEFROM_ftp = $_POST['DATEFROM'];
-                                        //echo '<br>';
-                                        $srch_DATETO_ftp = $_POST['DATETO'];
-                                        //echo '<hr>';
-                                        //Get previous Date From-----------------------
-                                        $srch_DATEFROM_ftp = strtotime($srch_DATEFROM_ftp);
-                                        $srch_DATEFROM_ftp = date('Y-m-d', $srch_DATEFROM_ftp);
-                                        //----------------------------------------------
-                                        //echo '<br>';
-                                        //Get previous Date To-----------------------
-                                        $srch_DATETO_ftp = strtotime($srch_DATETO_ftp);
-                                        $srch_DATETO_ftp = date('Y-m-d', $srch_DATETO_ftp);
-                                        //----------------------------------------------
-                                    }
-                                    //exit;
-                                    ?>
-                                    <div class="container" id="filters">
-                                        <form name="frmSRCH" class="form-horizontal" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>?sts=<?php echo $sts; ?>" method="post">
-                                            <div class="form-group al-right">
-                                                <label class="col-lg-1 control-label" style="padding:10px;">Dated :</label>
-                                                <div class="col-lg-8">
-                                                    <div class="input-daterange input-group">
-                                                        <input type="date" class="form-control" name="DATEFROM" placeholder="Start Date" autocomplete="off" value="<?php echo $srch_DATEFROM_ftp; ?>" />
-                                                        <span class="input-group-addon">to</span>
-                                                        <input type="date" class="form-control input-date-picker" id="DATETO" name="DATETO" placeholder="End Date" autocomplete="off" value="<?php echo $srch_DATETO_ftp; ?>" />
-                                                    </div>
-                                                </div>
-                                                <div class="col-lg-2">
-                                                    <button type="submit" name="srchfilter" class="btn btn-success btn-block">Search <i class="fa fa-search"></i></button>
-                                                </div>
-                                                <div class="col-lg-1">
-                                                    <a href="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" class="btn btn-warning"><i class="fa fa-times"></i></a>
-                                                </div>
-                                            </div>
-                                        </form>
+                                <?php if (isset($_GET['delete'])) { ?>
+                                    <div class="alert alert-danger">
+                                        <div class="container"><strong>Row is successfully deleted!</strong></div>
                                     </div>
-                                    <?php if (isset($_GET['delete'])) { ?>
-                                        <div class="alert alert-danger">
-                                            <div class="container"><strong>Row is successfully deleted!</strong></div>
-                                        </div>
-                                    <?php } ?>
-
-                                    <table id="leads" class="col-lg-12 table-striped table-condensed cf tbl">
-                                        <thead class="cf">
-                                            <tr>
-                                                <th>#</th>
-                                                <th>DATED</th>
-                                                <th>Lead Category</th>
-                                                <th>Lead Date</th>
-                                                <th>Lead Report</th>
-                                                <th>Status</th>
-                                                <th class="al-center">Action</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <?php
-                                            if (isset($_POST['srchfilter'])) //submit button name
-                                            {
-                                                $srch_DATEFROM = $_POST['DATEFROM'];
-                                                //echo '<br>';
-                                                $srch_DATETO = $_POST['DATETO'];
-                                                //echo '<hr>';
-                                                //Get previous Date From-----------------------
-                                                $srch_DATEFROM = strtotime($srch_DATEFROM);
-                                                //$srch_DATEFROM = strtotime('-1 day', $srch_DATEFROM);
-                                                $srch_DATEFROM = date('Y-m-d', $srch_DATEFROM);
-                                                //----------------------------------------------
-                                                //echo '<br>';
-                                                //Get previous Date To-----------------------
-                                                $srch_DATETO = strtotime($srch_DATETO);
-                                                $srch_DATETO = strtotime('+1 day', $srch_DATETO);
-                                                $srch_DATETO = date('Y-m-d', $srch_DATETO);
-                                                //----------------------------------------------
-
-                                                if ($srch_DATEFROM != NULL && $srch_DATETO != NULL) {
-                                                    if ($sts == 1) {
-                                                        $sts_query = "AND `STATUS`='1'";
-                                                    } else if ($sts == 2) {
-                                                        $sts_query = "AND `STATUS`='0'";
-                                                    } else {
-                                                        $sts_query = '';
-                                                    }
-                                                    $result = mysqli_query($link, "SELECT * FROM `calling_lead_title` WHERE DATED BETWEEN '$srch_DATEFROM' AND '$srch_DATETO' $sts_query ORDER BY DATED");
-                                                }
-                                            } else {
-                                                if ($sts == 1) {
-                                                    $result = mysqli_query($link, "SELECT * FROM `calling_lead_title` WHERE STATUS='1' ORDER BY DATED DESC");
-                                                } elseif ($sts == 2) {
-                                                    $result = mysqli_query($link, "SELECT * FROM `calling_lead_title` WHERE STATUS='0' ORDER BY DATED DESC");
-                                                } else {
-                                                    $result = mysqli_query($link, "SELECT * FROM `calling_lead_title` WHERE STATUS='1' ORDER BY DATED DESC");
-                                                }
-                                            }
-                                            $counters = '0';
-                                            while ($row = mysqli_fetch_array($result)) {
-                                                $ID = $row['ID'];
-                                                $DATED = $row['DATED'];
-                                                $LEAD_CATEGORY = $row['LEAD_CATEGORY'];
-                                                $LEAD_DATE = $row['LEAD_DATE'];
-                                                $STATUS = $row['STATUS'];
-                                                $counters = $counters + 1;
-                                                $result_calling_lead = mysqli_num_rows(mysqli_query($link, "SELECT * FROM calling_lead WHERE LEADTID='$ID' AND STATUS='1'"));
-                                                $results = mysqli_query($link, "SELECT * FROM calling_lead WHERE LEADTID='$ID'");
-                                                $result_count = mysqli_num_rows($results);
-                                                $results1 = mysqli_query($link, "SELECT * FROM calling_lead WHERE LEADTID='$ID' AND STATUS ='1'");
-                                                $result_count1 = mysqli_num_rows($results1);
-                                            ?>
-                                                <tr>
-                                                    <td></td>
-                                                    <td><?php echo $DATED; ?></td>
-                                                    <td><?php echo $LEAD_CATEGORY; ?></td>
-                                                    <td><?php echo $LEAD_DATE; ?></td>
-                                                    <td><?php echo $result_count1; ?>/<?php echo $result_count; ?></td>
-                                                    <td><?php if ($STATUS == '1') { ?><span class="bg-green text-white" style="padding: 5px; border-radius: 5px;">Active</span><?php } else { ?><span class="bg-red text-white" style="padding: 5px;border-radius: 5px;">InActive</span><?php } ?></td>
-                                                    <td data-title="Action" class="al-center"><a href="calling_lead_details.php?id=<?php echo $ID; ?>" data-toggle="tooltip" data-placement="top" title="Edit" class="btn btn-success btn-sm"><i class="fa fa-eye"></i></a><a href="calling_lead_edit.php?id=<?php echo $ID; ?>" data-toggle="tooltip" data-placement="top" title="Edit" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i></a><?php if ($result_calling_lead == 0) { ?><a href="<?php echo basename($_SERVER['PHP_SELF']) . "?del=" . $ID ?>" onclick="javascript:return confirm('Are you sure you want to delete ?')" data-toggle="tooltip" data-placement="top" title="Delete" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a><?php } ?></td>
-                                                </tr>
-                                            <?php } ?>
-                                        </tbody>
-                                    </table>
+                                <?php } ?>
+                                <div class="x_content">
+                                    <div id="calling-lead">
+                                        <div id="calling-loader">Loading.... Please Wait</div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -342,3 +233,11 @@ if (isset($_REQUEST['actc'])) {
         </div>
     </div>
     <?php include('inc_foot.php'); ?>
+    <script>
+        $(document).ready(function() {
+            $('#calling-lead').load(`ajax_load/calling_lead.php?sts=<?php echo $sts; ?>`, function() {
+                $('#leads').DataTable();
+                $('#calling-loader').hide();
+            });
+        });
+    </script>
